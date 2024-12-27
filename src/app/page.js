b85218464e3f22be581bfcd0321dc4f50949073e
msun97 <godqhr9782@naver.com>
2024-12-27 09:54:06 +0900

    Initial commit from Create Next App

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <div className="container">
+      <div className="logo">
+        <h1>PORTFOLIO</h1>
+      </div>
+      
+    </div>
+    </>
